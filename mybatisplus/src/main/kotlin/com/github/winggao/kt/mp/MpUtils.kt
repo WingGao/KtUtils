6c4ac6ab541b8126843d2ab9ca81d6823edc59b9
WingGao <wing.gao@live.com>
2022-12-23 19:42:51 +0800

    feat: 添加setSql

--- mybatisplus/src/main/kotlin/com/github/winggao/kt/mp/MpUtils.kt
+++ mybatisplus/src/main/kotlin/com/github/winggao/kt/mp/MpUtils.kt
@@ -4,13 +4,21 @@ import cn.hutool.core.util.ReflectUtil
 import com.baomidou.mybatisplus.core.conditions.SharedString
 import com.baomidou.mybatisplus.core.metadata.IPage
 import com.baomidou.mybatisplus.core.metadata.OrderItem
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper
 import com.baomidou.mybatisplus.core.toolkit.StringPool
 import com.baomidou.mybatisplus.extension.conditions.query.ChainQuery
 import com.baomidou.mybatisplus.extension.kotlin.KtQueryWrapper
+import com.baomidou.mybatisplus.extension.kotlin.KtUpdateChainWrapper
+import com.baomidou.mybatisplus.extension.kotlin.KtUpdateWrapper
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page
 import com.github.winggao.kt.request.WPage
 import com.github.winggao.kt.request.WPageT
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.math.min
+import kotlin.reflect.KClass
+import kotlin.reflect.KMutableProperty1
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
 
 private fun getSqlSelectField(q: KtQueryWrapper<*>): SharedString {
     return ReflectUtil.getFieldValue(q, "sqlSelect") as SharedString
@@ -23,6 +31,16 @@ fun <T : Any> KtQueryWrapper<T>.select(vararg columns: String): KtQueryWrapper<T
     if (columns.isNotEmpty()) {
         getSqlSelectField(this).stringValue = columns.joinToString(StringPool.COMMA)
     }
+    return this
+}
+
+fun <T : Any> KtUpdateWrapper<T>.setSql(column: KProperty1<T, *>, sql: String): KtUpdateWrapper<T> {
+    this.setSql("${this.columnsToString(true, column)}=$sql")
+    return this
+}
+
+fun <T : Any> KtUpdateChainWrapper<T>.setSql(column: KProperty1<T, *>, sql: String): KtUpdateChainWrapper<T> {
+    (this.wrapper as KtUpdateWrapper<T>).setSql(column, sql)
     return this
 }
 
@@ -69,3 +87,36 @@ fun WPage.getMpOrders(allowMap: Map<String, String>): List<OrderItem> {
 }
 
 //endregion
+
+object MpUtils {
+    // key1=table名 key2=propName val=colName 保存当前的所有表的类型
+    private val tableColumnMap = ConcurrentHashMap<String, Map<String, String>>()
+
+    /**
+     * 优化select
+     * @param dtoPropName 如果为null，则表示直接根据sqlTableAlias选择propNames；如果不为null，则表示选出的props属于dtoPropName
+     * @param propNames 要选择的字段，为空则全部
+     */
+    fun <C : Any> selectToPropSql(dtoPropName: String?, propTableClass: KClass<C>, sqlTableAlias: String, vararg propNames: KMutableProperty1<C, *>): String {
+        val tableInfo = TableInfoHelper.getTableInfo(propTableClass.java) ?: return ""
+        // 获取缓存
+        var colMap = tableColumnMap[tableInfo.tableName]
+        if (colMap == null) {
+            colMap = tableInfo.fieldList.associate { v -> v.property to v.column }.toMutableMap()
+            colMap[tableInfo.keyProperty] = tableInfo.keyColumn //添加主键
+            tableColumnMap[tableInfo.tableName] = colMap
+        }
+        val sqlB = StringBuilder()
+        var propNameList = propNames.toList()
+        if (propNames.isEmpty()) {
+            propNameList = propTableClass.memberProperties.toList() as List<KMutableProperty1<C, *>>
+        }
+        propNameList.forEachIndexed { idx, p ->
+            val col = colMap[p.name] ?: return@forEachIndexed
+            if (idx > 0) sqlB.append(",")
+            if (dtoPropName == null) sqlB.append("$sqlTableAlias.`${col}`")
+            else sqlB.append("$sqlTableAlias.`${col}` as `${dtoPropName}.${p.name}`")
+        }
+        return sqlB.toString()
+    }
+}
--- mybatisplus/src/main/kotlin/com/github/winggao/kt/mp/MpjExts.kt
+++ mybatisplus/src/main/kotlin/com/github/winggao/kt/mp/MpjExts.kt
@@ -3,6 +3,9 @@ package com.github.winggao.kt.mp
 import cn.hutool.core.util.ReflectUtil
 import com.baomidou.mybatisplus.core.conditions.SharedString
 import com.github.yulichang.query.MPJQueryWrapper
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
+import kotlin.reflect.KProperty1
 
 /**
  * 清除select信息
@@ -10,4 +13,11 @@ import com.github.yulichang.query.MPJQueryWrapper
 fun <T> MPJQueryWrapper<T>.clearSelect() {
     (ReflectUtil.getFieldValue(this, "selectColumns") as MutableList<String>).clear()
     (ReflectUtil.getFieldValue(this, "sqlSelect") as SharedString).toNull()
+}
+
+/**
+ * 根据prop选择
+ */
+fun <T, C : Any> MPJQueryWrapper<T>.select(tbClass: KClass<C>, tbAlias: String, vararg props: KProperty1<*, C>) {
+
 }
